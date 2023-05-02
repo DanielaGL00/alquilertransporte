0096973791e322c5879ee3a77796a30284ef06af
Daniela Garzón <danielagarzonlopez@gmail.com>
2023-05-02 02:19:19 -0500

    Modificación del DOM, JSON y Storage

--- Javascript/alquiler.js
+++ Javascript/alquiler.js
@@ -1,49 +1,50 @@
 // Definir los valores por minuto de servicio y valor por kilómetro recorrido para cada tipo de vehículo
-const valorPorMinutoServicio = [
-    0,
-    601,
-    541,
-   555,
-   823,
-   823,
-   923,
-   1030,
-   1917
-  ];
-  
-  const valorPorKilometroRecorrido = [
-    0,
-    1042,
-   1086,
-   1143,
-   978,
-   1233,
-   1295,
-   1550,
-   2751
-  ];
-  
-  // Capturar la opción seleccionada por el usuario (valor por minuto de servicio o valor por kilómetro recorrido)
-  const opcion = parseInt(prompt("Selecciona una opción:\n1. Valor por minuto de servicio\n2. Valor por kilómetro recorrido"));
-  
-  // Verificar la opción seleccionada y calcular el costo de alquiler
-  let costoTotal = 0;
-  
-  if (opcion === 1) {
-    // Calcular el costo de alquiler basado en el valor por minuto de servicio
-    const tipoVehiculo = parseInt(prompt("Selecciona un tipo de vehículo:\n1. Camioneta\n2. Minivan\n3. Campero\n4. Hibrido\n5. Camioneta con platón\n6. Microbus de 12 pasajero\n7. Microbus de 19 pasajero\n8. Bus"));
-    const minutos = parseInt(prompt("Ingrese la cantidad de minutos de alquiler"));
-    
-    costoTotal = valorPorMinutoServicio[tipoVehiculo] * minutos;
-  } else if (opcion === 2) {
-    // Calcular el costo de alquiler basado en el valor por kilómetro recorrido
-    const tipoVehiculo = parseInt(prompt("Selecciona un tipo de vehículo:\n1. Camioneta\n2. Minivan\n3. Campero\n4. Hibrido\n5. Camioneta con platón\n6. Microbus de 12 pasajero\n7. Microbus de 19 pasajero\n8. Bus"));
-    const kilometros = parseFloat(prompt("Ingrese la cantidad de kilómetros recorridos"));
-    
-    costoTotal = valorPorKilometroRecorrido[tipoVehiculo] * kilometros;
+const valorPorMinutoServicio = {
+  "1": 601,
+  "2": 541,
+  "3": 555,
+  "4": 823,
+  "5": 823,
+  "6": 923,
+  "7": 1030,
+  "8": 1917
+};
+
+const valorPorKilometroRecorrido = {
+  "1": 1042,
+  "2": 1086,
+  "3": 1143,
+  "4": 978,
+  "5": 1233,
+  "6": 1295,
+  "7": 1550,
+  "8": 2751
+};
+
+// Guardar los valores en el localStorage
+localStorage.setItem("valorPorMinutoServicio", JSON.stringify(valorPorMinutoServicio));
+localStorage.setItem("valorPorKilometroRecorrido", JSON.stringify(valorPorKilometroRecorrido));
+
+// Obtener los elementos del DOM
+const optionRadios = document.getElementsByName("option");
+const tipoVehiculoSelect = document.getElementById("tipoVehiculo");
+const minutosInput = document.getElementById("minutos");
+const kilometrosInput = document.getElementById("kilometros");
+const calcularButton = document.getElementById("calcular");
+const resultadoP = document.getElementById("resultado");
+
+
+// Detectar el evento de click en el botón de calcular
+calcularButton.addEventListener("click", function() {
+
+  let valor;
+  if (optionRadios[0].checked) {
+  valor = valorPorMinutoServicio[tipoVehiculoSelect.value] * minutosInput.value;
+  } else if (optionRadios[1].checked) {
+  valor = valorPorKilometroRecorrido[tipoVehiculoSelect.value] * kilometrosInput.value;
+  } else {
+  valor = 0;
   }
-  
-  // Mostrar el costo total de alquiler
-  console.log("El costo total de alquiler es: $" + costoTotal);
-  alert("El costo total de alquiler es: $" + costoTotal)
-  
+  // Mostrar el resultado en el elemento HTML correspondiente
+  resultadoP.innerHTML = "El costo de alquiler es de $" + valor;
+  });
